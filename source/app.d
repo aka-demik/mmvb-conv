b1f9c82b79c38dac0e85994984840b986781869e
Denis.Hlyakin <mail4aka@gmail.com>
2015-06-25 12:23:59 +0300

    Начнём пожалуй.

--- source/app.d
+++ source/app.d
@@ -5,54 +5,23 @@ import std.algorithm;
 import std.conv;
 import std.datetime;
 import std.range;
-import clipboard;
+import ae.sys.clipboard;
 
-
-private enum sBuy = x"CA F3 EF EB FF"c;        //Купля
-private enum sSell = x"CF F0 EE E4 E0 E6 E0"c; //Продажа
-//Номер заявки 	Код бумаги 	Направление 	МинДата и время заключения сделки 	Кол-во ЦБ	Кол-во 	Номер сделки 	 Сумма сделки расчет
-//13464857491	SBER		Купля		14.05.15 15:05				1		1	747,8
+private enum sBuy = "Купля";
+private enum sSell = "Продажа";
 
 private struct MyKey {
 	string instrument;
 	string oper;
 	string lot;
+	string botName;	
 }
 
 private struct MyOrder {
 	string open;
 	int n;
-	string instrument;
-	string lot;
-	SysTime openTime;
 }
 alias MyOrders = MyOrder[];
-
-/// Отчет ММВБ - Исх. Боты
-private struct BotSource {
-	string name;
-	string strategy;
-	SysTime openTime;
-	string instrument;
-	string lot;
-}
-
-private immutable static BotSource[] bots;
-
-BotSource[] getBotsForOrder(in MyOrder o) {
-	return bots.filter!(
-		(a) =>
-			o.instrument == a.instrument &&
-			o.lot == a.lot &&
-			abs(o.openTime - a.openTime).total!"seconds" < 5)
-		.array().dup();
-}
-
-string toString(in BotSource[] b) {
-	if (!b.length)
-		return "0\t\t\t";
-	return format("%s\t%s\t%s\t", b.length, b[0].name, b[0].strategy);
-}
 
 private string antiOper(in string oper) {
 	switch (oper) {
@@ -81,30 +50,29 @@ private SysTime excelStrToTime(in string s) {
 		0));
 }
 
-static this() {
-	foreach (e; File("source-botes.txt", "rb").byLineCopy().drop(1)) {
-		string[] cols = e.splitter("\t").array();
-		const tmp = BotSource(
-			cols[0],
-			cols[12],
-			excelStrToTime(cols[16]),
-			cols[18],
-			cols[15]);
-		bots ~= tmp;
-	}
-}
-
+//00-Номер заявки 	
+//01-Код бумаги 	
+//02-Направление 	
+//03-МинДата и время заключения сделки 	
+//04-Кол-во ЦБ	
+//05-Кол-во 	
+//06-Номер сделки 	 
+//07-Сумма сделки расчет
+//08-Количество ботов
+//09-Имя бота
+//10-Стратегия
+//13464857491	SBER	Купля	14.05.15 15:05	1	1	747,8
 void main() {
 	int counter;
 	string result;
-	auto srcS = getTextClipBoard();
+	auto srcS = getClipboardText();
 	string[] src = srcS.splitter("\r\n").filter!"a.length".array();
 
 	MyOrders[MyKey] orders;
 	foreach(e; src) {
 		string[] cols = e.splitter("\t").array();
-		auto keyo = MyKey(cols[1],         (cols[2]), cols[4]);
-		auto keyc = MyKey(cols[1], antiOper(cols[2]), cols[4]);
+		const keyo = MyKey(cols[1],         (cols[2]), cols[4], cols[9]);
+		const keyc = MyKey(cols[1], antiOper(cols[2]), cols[4], cols[9]);
 
 		if (keyc in orders) { // Если есть что закрывать
 			auto ordrs = orders[keyc];
@@ -115,14 +83,9 @@ void main() {
 			else
 				orders.remove(keyc);
 			result ~= format("%s\t%s\topen\t", order.open, order.n);
-			result ~= format("%s\t%s\tclose\t%s\r\n", e, order.n, toString(getBotsForOrder(order)));
+			result ~= format("%s\t%s\tclose\r\n", e, order.n);
 		} else {
-			const tmp = MyOrder(
-				e,
-				++counter,
-				cols[1],
-				cols[4],
-				excelStrToTime(cols[3]));
+			const tmp = MyOrder(e, ++counter);
 			if (keyo in orders)
 				orders[keyo] ~= tmp;
 			else
@@ -131,9 +94,9 @@ void main() {
 	}
 	foreach(v; orders.byValue())
 		foreach(e; v)
-			result ~= format("%s\t%s\topened\t\t\t\t\t\t\t\t\t\t%s\r\n", e.open, e.n, toString(getBotsForOrder(e)));
+			result ~= format("%s\t%s\topened\r\n", e.open, e.n);
 
-	setTextClipboard(result);
+	setClipboardText(result);
 	writeln("Done");
 	readln();
 }
